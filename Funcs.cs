c2c1d215a25191e4b986386fc881ff0b59c3e9d6
imnotanderson <imnotanderson@users.noreply.github.com>
2017-05-15 21:17:33 -0500

    Add files via upload

---
+++ Funcs.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Funcs : MonoBehaviour {
+
+    void Awake() {
+        Application.RegisterLogCallback((a, b, c) => {
+            str += a + b + c + "\n";
+        });
+        UnityKeyboardHelper.Init(UnityKeyboardHelper.MODE.RECV_PERC,null);
+
+        UnityKeyboardHelper.SetTextChangeListener(OnEditChange);
+    }
+
+    void OnEditChange(string str) {
+        if (MyUIInput.current!=null)
+        {
+            if (MyUIInput.current.value != str)
+            {
+                MyUIInput.current.value = str;
+            }
+        }
+    }
+ 
+    string str = "";
+    void OnGUI() { 
+        GUILayout.Label(str);
+    }
+
+    public void Clear() {
+        str = "";
+    }
+
+    public void Hide() {
+        UnityKeyboardHelper.Hide();
+    }
+}
